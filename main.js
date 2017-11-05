82de3840f5c24658935cc800680d81163196bf7c
jahidul islam <jahidulislam1153@gmail.com>
2017-11-06 05:28:43 +0600

    deletation of main js fixed

--- main.js
+++ main.js
@@ -29,8 +29,11 @@ function addlist(e) {
         localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     }
     showlist();
+    document.getElementById("sname").value = "";
+    document.getElementById("surl").value = "";
 
 }
+
 
 function showlist() {
     if (localStorage.getItem("bookmarks") != null) {
@@ -40,21 +43,34 @@ function showlist() {
         show.innerHTML = ""
         for (var i = 0; i < len; i++) {
             //console.log(bookmarks[i].name);
+            var surl = bookmarks[i].surl;
             show.innerHTML += "<div class='list'><h3>" + bookmarks[i].name + "</h3><br>";
             show.innerHTML += "<p class='text-danger'>" + bookmarks[i].surl + "</p>";
-            show.innerHTML += "<a class='btn btn-danger' id='del' onclick='del()' title='" + bookmarks[i].surl + "'>" + bookmarks[i].surl + "</a>";
+            show.innerHTML += "<a class='btn btn-danger' id='del" + i + "' onclick='del(del" + i + ")' title='" + bookmarks[i].surl + "'>" + bookmarks[i].surl + "</a>";
         }
     } else {
         return false;
     }
 }
 
-function del() {
-    var title = document.getElementById("del").title;
+function del(ar) {
+    //var tit = ar;
+    //console.log(ar);
+    var title = ar.getAttribute("title");
+    //console.log(id);
     bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
     for (var i = 0; i < bookmarks.length; i++) {
-        if (title == bookmarks[i].surl) {
-            console.log(bookmarks[i].surl);
+        //console.log(i);
+        var st = bookmarks[i].surl;
+        //st = st.toString();
+        if (st == title) {
+
+            //console.log(title);
+            //console.log(st);
+            //console.log(st);
+            //console.log(bookmarks[i].surl);
+            //console.log(bookmarks[i].surl);
+            //console.log(i);
             bookmarks.splice(i, 1);
             localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
         }
